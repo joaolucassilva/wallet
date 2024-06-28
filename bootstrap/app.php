c0d704e12c456e7ef40224bc5161d3b748a333dc
João Lucas <luccas332@gmail.com>
2024-06-27 22:20:44 -0300

    feat: initial settings (#1)
    * feat: add initial settings
    
    * style: format code with psr12
    
    * ci: add mysql and redis connection ci tests
    
    * style: phpmd supress warning static access and camel case names
    
    * fix: rename env.testing used to ci in env.ci

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        commands: __DIR__ . '/../routes/console.php',
+        then: function () {
+            Route::prefix('v1')
+                ->middleware('api')
+                ->name('v1.')
+                ->group(base_path('routes/v1.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
